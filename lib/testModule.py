9584a6e1f8453a8b51ce9a8057604a58026cf60a
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2016-02-08 17:21:46 +0100

    added simple module

---
+++ lib/testModule.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Example of a simple module.
+
+
+
+
+"""
+
+def printFoo():
+    """ print 'foo', regardless input """
+    print("foo")
+    
+
+def addFun(s):
+    """
+    makes everything fun
+    
+    Parameters
+    --------------
+    s : str
+        input string
+        
+    Returns 
+    ----------
+    str : new string
+    
+    """
+    
+    return s+" is fun!"
